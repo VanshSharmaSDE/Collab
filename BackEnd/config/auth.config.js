8a5dddcbc3290110918ff9d74925345c77b73db3
Vansh Sharma <vansh4664@gmail.com>
2024-12-28 12:00:58 +0530

    Delete BackEnd directory

--- BackEnd/config/auth.config.js
+++
@@ -1,7 +0,0 @@
-// config/auth.config.js
-module.exports = {
-    jwtSecret: process.env.JWT_SECRET || 'your-secret-key',
-    jwtExpiration: 86400, // 24 hours
-    refreshTokenExpiration: 604800, // 7 days
-  };
-  
